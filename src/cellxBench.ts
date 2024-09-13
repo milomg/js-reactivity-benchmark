c931f71116315a9fe7276b91ee9504993f67e974
Travis Fischer <fisch0920@gmail.com>
2024-09-13 06:09:09 -0500

    chore: remove extra debug logs

--- src/cellxBench.ts
+++ src/cellxBench.ts
@@ -52,13 +52,10 @@ const cellx = (framework: ReactiveFramework, layers: number) => {
     ] as const;
 
     framework.withBatch(() => {
-      // console.log(framework.name, 'batch', 0)
       start.prop1.write(4);
-      // console.log(framework.name, 'batch', 1)
       start.prop2.write(3);
       start.prop3.write(2);
       start.prop4.write(1);
-      // console.log(framework.name, 'batch', 2)
     });
 
     const after = [
@@ -68,7 +65,6 @@ const cellx = (framework: ReactiveFramework, layers: number) => {
       end.prop4.read(),
     ] as const;
 
-    // console.log(framework.name, 4)
     const endTime = performance.now();
     const elapsedTime = endTime - startTime;
 
