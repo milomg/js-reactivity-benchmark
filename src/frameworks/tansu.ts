ddb950154bdf33491060317534a6fa4c33109f98
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2025-03-06 09:23:19 +0100

    add Tansu framework (missing adapter file)

---
+++ src/frameworks/tansu.ts
@@ -0,0 +1,22 @@
+import { ReactiveFramework } from "../util/reactiveFramework";
+import { writable, computed, batch } from "@amadeus-it-group/tansu";
+
+export const tansuFramework: ReactiveFramework = {
+  name: "@amadeus-it-group/tansu",
+  signal: (initialValue) => {
+    const w = writable(initialValue);
+    return {
+      write: (v) => w.set(v),
+      read: () => w(),
+    };
+  },
+  computed: (fn) => {
+    const c = computed(fn);
+    return {
+      read: () => c(),
+    };
+  },
+  effect: (fn) => computed(fn).subscribe(() => {}),
+  withBatch: (fn) => batch(fn),
+  withBuild: (fn) => fn(),
+};
