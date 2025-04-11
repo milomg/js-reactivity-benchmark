7a49e2b1ed23b4708a8b29f1eeb524ff21208271
Milo <milomg4@gmail.com>
2025-04-11 14:20:17 -0400

    enable graphing

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -6,6 +6,9 @@ import {
   perfResultHeaders,
   runTests,
 } from "js-reactivity-benchmark/src/index";
+import * as d3 from "d3";
+
+let data: PerfResult[] = [];
 
 const pre = document.querySelector("pre")!;
 function logLine(line: string): void {
@@ -13,7 +16,84 @@ function logLine(line: string): void {
 }
 
 function logPerfResult(result: PerfResult): void {
+  data.push(result);
   logLine(formatPerfResult(result));
+}
+
+function graph() {
+  console.log(data);
+
+  document.querySelector("svg")?.remove();
+
+  const width = 928;
+  const marginTop = 10;
+  const marginRight = 10;
+  const marginBottom = 20;
+  const marginLeft = 120;
+
+  const tests = new Set(data.map((d) => d.test));
+  const frameworks = new Set(data.map((d) => d.framework));
+
+  const testGroupHeight = frameworks.size * 20 + 10;
+  const height = marginTop + tests.size * testGroupHeight + marginBottom;
+
+  const fy = d3
+    .scaleBand()
+    .domain(tests)
+    .rangeRound([marginTop, height - marginBottom])
+    .paddingInner(0.1);
+
+  const y = d3
+    .scaleBand()
+    .domain(frameworks)
+    .rangeRound([0, fy.bandwidth()])
+    .padding(0.05);
+
+  const color = d3
+    .scaleOrdinal<string>()
+    .domain(frameworks)
+    .range(d3.schemeSpectral[Math.max(frameworks.size, 3)])
+    .unknown("#ccc");
+
+  const x = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.time)!])
+    .nice()
+    .rangeRound([marginLeft, width - marginRight]);
+
+  const svg = d3
+    .create("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("style", "max-width: 100%; height: auto;");
+
+  svg
+    .selectAll()
+    .data(d3.group(data, (d) => d.test))
+    .join("g")
+    .attr("transform", ([test]) => `translate(0,${fy(test)})`)
+    .selectAll()
+    .data(([, d]) => d)
+    .join("rect")
+    .attr("x", x(0)!)
+    .attr("y", (d) => y(d.framework)!)
+    .attr("width", (d) => x(d.time)! - x(0)!)
+    .attr("height", y.bandwidth())
+    .attr("fill", (d) => color(d.framework));
+
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80, "s"))
+    .call((g) => g.select(".domain").remove());
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(fy).tickSizeOuter(0))
+    .call((g) => g.selectAll(".domain").remove());
+
+  pre.after(svg.node()!);
 }
 
 async function main() {
@@ -22,3 +102,4 @@ async function main() {
 }
 
 (window as any).main = main;
+(window as any).graph = graph;
