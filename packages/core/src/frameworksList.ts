3243bad56cf941e3bb4f450af02bc5143817c4cb
Milo <milomg4@gmail.com>
2025-04-16 17:41:27 -0400

    temporarily disable oby

--- packages/core/src/frameworksList.ts
+++ packages/core/src/frameworksList.ts
@@ -3,7 +3,6 @@ import { alienFramework } from "./frameworks/alienSignals";
 import { angularFramework as angularFramework2 } from "./frameworks/angularSignals2";
 import { compostateFramework } from "./frameworks/inactive/compostate";
 import { mobxFramework } from "./frameworks/inactive/mobx";
-import { obyFramework } from "./frameworks/inactive/oby";
 import { preactSignalFramework } from "./frameworks/preactSignals";
 import { reactivelyFramework } from "./frameworks/reactively";
 import { sFramework } from "./frameworks/inactive/s";
@@ -20,12 +19,14 @@ import { xReactivityFramework } from "./frameworks/xReactivity";
 // Disabled until cleanup performance is fixed
 // import { tc39SignalsFramework } from "./frameworks/tc39signals";
 
+// Currently failing cellx tests
+// import { obyFramework } from "./frameworks/inactive/oby";
+
 export const frameworkInfo: FrameworkInfo[] = [
-  { framework: angularFramework2, testPullCounts: true },
   { framework: alienFramework, testPullCounts: true },
+  { framework: angularFramework2, testPullCounts: true },
   { framework: compostateFramework, testPullCounts: true },
   { framework: mobxFramework, testPullCounts: true },
-  { framework: obyFramework, testPullCounts: true },
   { framework: preactSignalFramework, testPullCounts: true },
   { framework: reactivelyFramework, testPullCounts: true },
   { framework: sFramework },
@@ -42,7 +43,7 @@ export const allFrameworks: FrameworkInfo[] = [
   { framework: compostateFramework, testPullCounts: true },
   { framework: mobxFramework, testPullCounts: true },
   // { framework: molWireFramework, testPullCounts: true },
-  { framework: obyFramework, testPullCounts: true },
+  // { framework: obyFramework, testPullCounts: true },
   { framework: preactSignalFramework, testPullCounts: true },
   { framework: reactivelyFramework, testPullCounts: true },
   { framework: sFramework },
