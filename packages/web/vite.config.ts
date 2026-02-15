56eb45e84b3f6fcfa867840725e66b59a9b7467a
Milo <milomg4@gmail.com>
2026-02-14 19:09:50 -0800

    fix ngDevMode, use vite-8

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,3 +1,20 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  define: {
+    ngDevMode: "false",
+  },
+  build: {
+    sourcemap: true,
+    rolldownOptions: {
+      output: {
+        minify: {
+          mangle: false,
+          codegen: {
+            removeWhitespace: false,
+          },
+        },
+      },
+    },
+  },
+});
